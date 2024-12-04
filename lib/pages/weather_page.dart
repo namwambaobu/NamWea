8e0a09a7f07269f1fcbac1d4a51fb19ccc887d5e
unknown <namwmambaobuyu25@gmail.com>
2024-12-04 15:28:50 +0300

    another final commit

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -58,6 +58,34 @@ class _WeatherPageState extends State<WeatherPage> {
     }
   }
 
+  //action animations
+  String getActionAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/rainy.json'; //default to sunny
+
+    switch (mainCondition.toLowerCase()) {
+      case 'cloud':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/rainy.json';
+
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/umbrella.json';
+
+      case 'thunderstorm':
+        return 'assets/rainy.json';
+
+      case 'clear':
+        return 'assets/sun.json';
+      default:
+        return 'assets/umbrella.json';
+    }
+  }
+
   //init state
   @override
   void initState() {
@@ -69,25 +97,33 @@ class _WeatherPageState extends State<WeatherPage> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.brown[200],
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.location_city),
-            //city name
-            Text(_weather?.cityName ?? "Loading city..."),
+      backgroundColor: Colors.grey[200],
+      body: ListView(children: [
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.location_city),
+              //city name
+              Text(_weather?.cityName ?? "Loading city..."),
 
-            //animation
-            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
-            //temperature
-            Text('${_weather?.temperature.round()}°C'),
+              //animation
+              Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+              //temperature
+              Text('${_weather?.temperature.round()}°C'),
 
-            //Animation
-            Text(_weather?.mainCondition ?? "")
-          ],
+              //Animation
+              Text(_weather?.mainCondition ?? "Loading..."),
+
+              //action Animations
+              Lottie.asset(getActionAnimation(_weather?.mainCondition)),
+
+              //don't forget to...
+              Text('Dont Forget to 👆😃')
+            ],
+          ),
         ),
-      ),
+      ]),
     );
   }
 }
